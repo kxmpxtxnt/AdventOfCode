23359106f9e18673796ddb32c0c6471f108fe401
Paul Kindler <pa179kin@gmail.com>
2024-12-02 11:20:29 +0100

    add second to main function

--- 2024/kotlin2024/src/main/kotlin/fyi/pauli/aoc/2024.kt
+++ 2024/kotlin2024/src/main/kotlin/fyi/pauli/aoc/2024.kt
@@ -3,9 +3,11 @@ package fyi.pauli.aoc
 import fyi.pauli.aoc.common.adventOfCode
 import fyi.pauli.aoc.common.plusAssign
 import fyi.pauli.aoc.days.first
+import fyi.pauli.aoc.days.second
 
 fun main(): Unit = adventOfCode(2024) {
   this += listOf(
     first,
+    second
   )
 }
