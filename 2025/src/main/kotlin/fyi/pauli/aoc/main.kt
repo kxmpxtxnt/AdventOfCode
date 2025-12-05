daf2115251da1f254d2e49e0ca088590b6a8e667
Paul K <pa179kin@gmail.com>
2025-12-05 16:07:25 +0100

    add solution for day 5

--- 2025/src/main/kotlin/fyi/pauli/aoc/main.kt
+++ 2025/src/main/kotlin/fyi/pauli/aoc/main.kt
@@ -1,10 +1,11 @@
 package fyi.pauli.aoc
 
-import fyi.pauli.aoc.days.day4
+import fyi.pauli.aoc.days.*
 
 fun main() {
     //day1()
     //day2()
     //day3()
-    day4()
+    //day4()
+    day5()
 }
